fe26b0d1ee43184e11069cad6e55f7c29f77657b
Adrysson <adrysson_santos@msn.com>
2019-04-10 22:44:28 -0300

    Criando página inicial

--- app/src/pages/Index.vue
+++ app/src/pages/Index.vue
@@ -1,7 +1,26 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
-  </q-page>
+    <q-page class="grid">
+        <div class="row justify-center">
+            <h2>My Test</h2>
+        </div>
+        <div class="row justify-center">
+            <q-card>
+                <q-card-title align="center">
+                    Escolha uma das opções
+                </q-card-title>
+                <q-card-separator />
+                <q-card-main>
+                  <div class="row justify-center">
+                      <q-btn label="Entrar" color="primary" />
+                  </div>
+                  <br>
+                  <div class="row justify-center">
+                      <q-btn label="Realizar cadastro" color="secondary" />
+                  </div>
+                </q-card-main>
+            </q-card>
+        </div>
+    </q-page>
 </template>
 
 <style>
